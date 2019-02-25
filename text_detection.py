2386662f058ec138355f8f3d2fa207bf4e77eb3f
danny crasto <danwald79@gmail.com>
2019-02-25 07:20:45 +0400

    renamed cmdline arg to match class kwarg

--- text_detection.py
+++ text_detection.py
@@ -76,7 +76,7 @@ ap.add_argument("-i", "--image", type=str,
 	help="path to input image")
 ap.add_argument("-east", "--east", type=str, default='./frozen_east_text_detection.pb',
 	help="path to input EAST text detector")
-ap.add_argument("-c", "--min-confidence", type=float, default=0.5,
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
 	help="minimum probability required to inspect a region")
 ap.add_argument("-w", "--width", type=int, default=320,
         help="resized image width (should be multiple of 32)")
@@ -137,7 +137,7 @@ print("[INFO] text detection took {:.6f} seconds".format(end - start))
 #import ipdb; ipdb.set_trace();
 # loop over the number of rows
 start = time.time()
-boxes = get_boxes(numRows, numCols, args['min_confidence'])
+boxes = get_boxes(numRows, numCols, args['confidence'])
 end = time.time()
 # loop over the bounding boxes
 print('Found {boxes} ROIs {seconds:.6f} seconds'.format(boxes=len(boxes),seconds=(end-start)))
