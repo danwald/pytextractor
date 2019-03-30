fd38b9591a32c5ff60dd864a8234b394ef202ffd
danny crasto <danwald79@gmail.com>
2019-03-30 14:17:36 +0400

    fixed tests assest using pkg_resource

--- tests/test_detector.py
+++ tests/test_detector.py
@@ -1,3 +1,5 @@
+from pkg_resources import resource_filename
+
 import pytest
 
 from pytextractor.pytextractor import PyTextractor
@@ -7,19 +9,18 @@ class TestDetector(object):
         self.extractor = PyTextractor()
 
     @pytest.mark.parametrize('image', [
-        ('assets/test_images/52.png'),
-        ('assets/test_images/53.png'),
-        ('assets/test_images/56.png'),
+        resource_filename(__name__, 'test_images/52.png'),
+        resource_filename(__name__, 'test_images/53.png'),
+        resource_filename(__name__, 'test_images/56.png'),
     ])
     def test_number_detector(self, image):
         detected = self.extractor.get_image_text(image, number=True)
         assert len(detected)
 
-
     @pytest.mark.parametrize('image,expected_number', [
-        ('assets/test_images/52.png', 52),
-        ('assets/test_images/53.png', 53),
-        ('assets/test_images/56.png', 56),
+        (resource_filename(__name__, 'test_images/52.png'), 52),
+        (resource_filename(__name__, 'test_images/53.png'), 53),
+        (resource_filename(__name__, 'test_images/56.png'), 56),
     ])
     def test_number_extractor(self, image, expected_number):
         detected = self.extractor.get_image_text(image, number=True)
@@ -28,7 +29,7 @@ class TestDetector(object):
 
     @pytest.mark.skip(reason="[LIMITATION] can't detect number")
     @pytest.mark.parametrize('image,expected_number', [
-        ('assets/test_images/57.png', 57),
+        (resource_filename(__name__, 'test_images/57.png'), 57),
     ])
     def test_number_extractor_tweaked(self, image, expected_number):
         detected = self.extractor.get_image_text(image, number=True, percentage=4)
