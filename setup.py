cce41135187f049cd27a406fc08a93ecc57a74d5
danny crasto <danwald79@gmail.com>
2019-03-30 14:23:11 +0400

    version bump 0.0.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [
 
 setup(
     name='pytextractor',
-    version='0.0.2',
+    version='0.0.3',
     author='danny crasto',
     author_email='danwald79@gmail.com',
     description='text extractor from images',
