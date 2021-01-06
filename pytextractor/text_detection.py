91c9c3b836f740fd5979e64adb040661b3046442
danny crasto <danwald79@gmail.com>
2021-01-06 20:53:05 +0400

    flaked

--- pytextractor/text_detection.py
+++ pytextractor/text_detection.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-from pkg_resources import resource_string
 import argparse
 import sys
 
@@ -10,26 +9,18 @@ from .pytextractor import PyTextractor
 def text_detector():
     # construct the argument parser and parse the arguments
     ap = argparse.ArgumentParser(description='Text/Number extractor from image')
-    ap.add_argument('images', type=str, nargs='+',
-            help='path(s) to input image(s)')
-    ap.add_argument( '--east', type=str, required=False,
-        help='path to input EAST text detector')
-    ap.add_argument('-c', '--confidence', type=float, default=0.5,
-        help='minimum probability required to inspect a region')
-    ap.add_argument('-w', '--width', type=int, default=320,
-        help='resized image width (should be multiple of 32)')
-    ap.add_argument('-e', '--height', type=int, default=320,
-        help='resized image height (should be multiple of 32)')
-    ap.add_argument('-d', '--display', action='store_true',
-        help='Display bounding boxes')
-    ap.add_argument('-n', '--numbers', action='store_true',
-        help='Detect only numbers')
-    ap.add_argument('-p', '--percentage', type=float, default=2.0,
-        help='Expand/shrink detected bound box')
-    ap.add_argument('-b', '--min-boxes', type=int, default=1,
-        help='minimum number of detected boxes to return')
-    ap.add_argument('-i', '--max-iterations', type=int, default=20,
-        help='max number of iterations finding min_boxes')
+    ap.add_argument('images', type=str, nargs='+', help='path(s) to input image(s)')
+    ap.add_argument('--east', type=str, required=False, help='path to input EAST text detector')
+    ap.add_argument(
+        '-c', '--confidence', type=float, default=0.5, help='minimum probability required to inspect a region'
+    )
+    ap.add_argument('-w', '--width', type=int, default=320, help='resized image width (should be multiple of 32)')
+    ap.add_argument('-e', '--height', type=int, default=320, help='resized image height (should be multiple of 32)')
+    ap.add_argument('-d', '--display', action='store_true', help='Display bounding boxes')
+    ap.add_argument('-n', '--numbers', action='store_true', help='Detect only numbers')
+    ap.add_argument('-p', '--percentage', type=float, default=2.0, help='Expand/shrink detected bound box')
+    ap.add_argument('-b', '--min-boxes', type=int, default=1, help='minimum number of detected boxes to return')
+    ap.add_argument('-i', '--max-iterations', type=int, default=20, help='max number of iterations finding min_boxes')
 
     kwargs = vars(ap.parse_args())
     images = kwargs.pop('images')
