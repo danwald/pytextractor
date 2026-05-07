959ccd2cdbb58b487f17867ec7217dbc1cf9bef3
danny crasto <danwald79@gmail.com>
2026-05-07 22:23:23 +0400

    fix tests ty checks

--- tests/test_detector.py
+++ tests/test_detector.py
@@ -1,7 +1,8 @@
-import re
 import os
+import re
 
 import pytest
+
 from pytextractor.pytextractor import PyTextractor
 
 _HERE = os.path.dirname(__file__)
@@ -11,43 +12,52 @@ def resource_filename(package, path):
     return os.path.join(_HERE, path)
 
 
-class TestDetector(object):
+class TestDetector:
     def setup_method(self, method):
         self.extractor = PyTextractor()
 
-    @pytest.mark.parametrize('image', [
-        resource_filename(__name__, 'test_images/52.png'),
-        resource_filename(__name__, 'test_images/53.png'),
-        resource_filename(__name__, 'test_images/56.png'),
-    ])
+    @pytest.mark.parametrize(
+        "image",
+        [
+            resource_filename(__name__, "test_images/52.png"),
+            resource_filename(__name__, "test_images/53.png"),
+            resource_filename(__name__, "test_images/56.png"),
+        ],
+    )
     def test_number_detector(self, image):
         detected = self.extractor.get_image_text(image, number=True)
         assert len(detected)
 
-    @pytest.mark.parametrize('image,expected_number', [
-        (resource_filename(__name__, 'test_images/52.png'), 52),
-    ])
+    @pytest.mark.parametrize(
+        "image,expected_number",
+        [
+            (resource_filename(__name__, "test_images/52.png"), 52),
+        ],
+    )
     def test_number_extractor(self, image, expected_number):
         detected = self.extractor.get_image_text(image, number=True)
-        assert(int(detected[0]) == expected_number)
-
+        assert int(detected[0]) == expected_number
 
     @pytest.mark.skip(reason="[LIMITATION] can't detect number")
-    @pytest.mark.parametrize('image,expected_number', [
-        (resource_filename(__name__, 'test_images/53.png'), 53),
-        (resource_filename(__name__, 'test_images/56.png'), 56),
-        (resource_filename(__name__, 'test_images/57.png'), 57),
-    ])
+    @pytest.mark.parametrize(
+        "image,expected_number",
+        [
+            (resource_filename(__name__, "test_images/53.png"), 53),
+            (resource_filename(__name__, "test_images/56.png"), 56),
+            (resource_filename(__name__, "test_images/57.png"), 57),
+        ],
+    )
     def test_number_extractor_tweaked(self, image, expected_number):
-        detected = self.extractor.get_image_text(image, number=True, percentage=4, min_boxes=2)
+        detected = self.extractor.get_image_text(
+            image, number=True, percentage=4, min_boxes=2
+        )
         for roi in detected:
             try:
-                print(roi, ' ? ', expected_number)
-                number = int(re.sub(r'\D', '', roi))
+                print(roi, " ? ", expected_number)
+                number = int(re.sub(r"\D", "", roi))
                 if number == expected_number:
                     break
             except ValueError:
                 pass
         else:
-            assert False
-
+            raise AssertionError("No ROI detected")
